b1d6bf434c1b0d5dfd9372ace5d094e2c23abd75
Mark Kenneth M. Bayona <mkmbayona@gmail.com>
2021-08-04 18:52:58 +0800

    Added constants file, Message model, UITableview with initial dataSource

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Mark Kenneth Bayona on 8/4/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
