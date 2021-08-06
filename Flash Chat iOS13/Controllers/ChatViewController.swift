a438963b31f9f7c255620107985a37c97e5b579f
Mark Kenneth M. Bayona <mkmbayona@gmail.com>
2021-08-06 16:52:41 +0800

    Get realtime data using listener, clear data to prevent duplicate cells

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -35,9 +35,9 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessages() {
-        messages = []
-        
-        db.collection(K.FStore.collectionName).getDocuments { querySnapshot, error in
+        db.collection(K.FStore.collectionName).addSnapshotListener { querySnapshot, error in
+            self.messages = []
+            
             if let e = error {
                 print("Error: \(e)")
             } else {
